8b1aefd8129a88635f4bb0ddea6ea061e388f0bf
Benny Neugebauer <bn@bennyn.de>
2019-04-29 12:20:05 +0200

    Handle return type

--- src/info/RequestMethod.ts
+++ src/info/RequestMethod.ts
@@ -1,11 +1,17 @@
 class RequestMethod {
   public method: string;
+  public returnType: string;
   public url: string;
 
   constructor(url: string, method: string) {
     this.url = url;
+    if (method === 'delete' || method === 'head') {
+      this.returnType = '';
+    } else {
+      this.returnType = '{}';
+    }
     this.method = method;
   }
 }
 
-export {RequestMethod}
+export {RequestMethod};
