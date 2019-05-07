8967cdd72948a2c1598e76baa59fe26aa4f8ab86
Florian Keller <ffflorian@users.noreply.github.com>
2019-05-07 14:22:17 +0200

    feat: Add CLI options (#4)

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,42 @@
+#!/usr/bin/env node
+
 import minimist from 'minimist';
 import path from 'path';
+
 import {writeClient} from './APIClientGenerator';
+const {bin, description, name, version} = require('../package.json');
+const binName = Object.keys(bin)[0] || name;
 
 const argv = minimist(process.argv.slice(1));
 
-const inputFile = path.resolve(path.join(process.cwd(), argv.i));
-const outputDirectory = path.resolve(path.join(process.cwd(), argv.o));
+const usage = `Usage: ${binName} [options]
+
+${description}
+
+Options:
+  -i, --input    Set the input file
+  -o, --output   Set the output directory
+  -h, --help     Output usage information
+  -v, --version  Output the version number
+`;
+
+const helpArg = argv.h || argv.help;
+const inputArg = argv.i || argv.input;
+const outputArg = argv.i || argv.output;
+const versionArg = argv.v || argv.version;
+
+if (versionArg) {
+  console.log(version);
+  process.exit();
+}
+
+if (helpArg || !inputArg || !outputArg) {
+  console.log(usage);
+  process.exit();
+}
+
+const inputFile = path.resolve(process.cwd(), inputArg);
+const outputDirectory = path.resolve(process.cwd(), outputArg);
 
 writeClient(inputFile, outputDirectory).catch(error => {
   console.error(error);
