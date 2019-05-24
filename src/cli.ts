1c1eb60ab40b0121c97abac9ed5aae13598a7332
Florian Keller <ffflorian@users.noreply.github.com>
2019-05-24 17:07:22 +0200

    chore: Use commander instead of caporal (#16)

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import program from 'caporal';
+import program from 'commander';
 import path from 'path';
 
 import {writeClient} from './Swaxios';
@@ -8,19 +8,22 @@ const {bin, description, name, version} = require('../package.json');
 const binName = Object.keys(bin)[0] || name;
 
 program
-  .name(`${name[0].toUpperCase()}${name.substring(1)}`)
-  .bin(binName)
+  .name(binName)
   .description(description)
-  .version(version)
-  .option('-i, --input <file>', 'Set the input file', undefined, undefined, true)
-  .option('-o, --output <directory>', 'Set the output directory', undefined, undefined, true)
-  .action((args, options) => {
-    const inputFile = path.resolve(process.cwd(), options.input);
-    const outputDirectory = path.resolve(process.cwd(), options.output);
-
-    writeClient(inputFile, outputDirectory).catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
-  })
+  .version(version, '-v, --version')
+  .option('-i, --input <file>', 'Set the input file')
+  .option('-o, --output <directory>', 'Set the output directory')
   .parse(process.argv);
+
+if (!program.input || !program.output) {
+  program.outputHelp();
+  process.exit(1);
+}
+
+const inputFile = path.resolve(process.cwd(), program.input);
+const outputDirectory = path.resolve(process.cwd(), program.output);
+
+writeClient(inputFile, outputDirectory).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
