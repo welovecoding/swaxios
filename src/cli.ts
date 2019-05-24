1ab4317f1614652cd741f518a01d7156350500f6
Florian Keller <florian.keller@wire.com>
2019-05-24 15:59:59 +0200

    feat: Use commander instead of minimist

--- src/cli.ts
+++ src/cli.ts
@@ -1,42 +1,27 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist';
+import program from 'commander';
 import path from 'path';
 
 import {writeClient} from './Swaxios';
 const {bin, description, name, version} = require('../package.json');
 const binName = Object.keys(bin)[0] || name;
 
-const argv = minimist(process.argv.slice(1));
+program
+  .name(binName)
+  .description(description)
+  .version(version, '-v, --version')
+  .option('-i, --input <file>', 'Set the input file')
+  .option('-o, --output <directory>', 'Set the output directory')
+  .parse(process.argv);
 
-const usage = `Usage: ${binName} [options]
-
-${description}
-
-Options:
-  -i, --input    Set the input file
-  -o, --output   Set the output directory
-  -h, --help     Output usage information
-  -v, --version  Output the version number
-`;
-
-const helpArg = argv.h || argv.help;
-const inputArg = argv.i || argv.input;
-const outputArg = argv.o || argv.output;
-const versionArg = argv.v || argv.version;
-
-if (versionArg) {
-  console.log(version);
-  process.exit();
+if (!program.input || !program.output) {
+  program.outputHelp();
+  process.exit(1);
 }
 
-if (helpArg || !inputArg || !outputArg) {
-  console.log(usage);
-  process.exit();
-}
-
-const inputFile = path.resolve(process.cwd(), inputArg);
-const outputDirectory = path.resolve(process.cwd(), outputArg);
+const inputFile = path.resolve(process.cwd(), program.input);
+const outputDirectory = path.resolve(process.cwd(), program.output);
 
 writeClient(inputFile, outputDirectory).catch(error => {
   console.error(error);
