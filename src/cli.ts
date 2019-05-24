84e3c04f79d9b76cdfbaa70fcc04dd0b578813ed
Florian Keller <ffflorian@users.noreply.github.com>
2019-05-24 16:39:58 +0200

    feat: Use caporal instead of minimist (#15)

--- src/cli.ts
+++ src/cli.ts
@@ -1,44 +1,26 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist';
+import program from 'caporal';
 import path from 'path';
 
 import {writeClient} from './Swaxios';
 const {bin, description, name, version} = require('../package.json');
 const binName = Object.keys(bin)[0] || name;
 
-const argv = minimist(process.argv.slice(1));
+program
+  .name(`${name[0].toUpperCase()}${name.substring(1)}`)
+  .bin(binName)
+  .description(description)
+  .version(version)
+  .option('-i, --input <file>', 'Set the input file', undefined, undefined, true)
+  .option('-o, --output <directory>', 'Set the output directory', undefined, undefined, true)
+  .action((args, options) => {
+    const inputFile = path.resolve(process.cwd(), options.input);
+    const outputDirectory = path.resolve(process.cwd(), options.output);
 
-const usage = `Usage: ${binName} [options]
-
-${description}
-
-Options:
-  -i, --input    Set the input file
-  -o, --output   Set the output directory
-  -h, --help     Output usage information
-  -v, --version  Output the version number
-`;
-
-const helpArg = argv.h || argv.help;
-const inputArg = argv.i || argv.input;
-const outputArg = argv.o || argv.output;
-const versionArg = argv.v || argv.version;
-
-if (versionArg) {
-  console.log(version);
-  process.exit();
-}
-
-if (helpArg || !inputArg || !outputArg) {
-  console.log(usage);
-  process.exit();
-}
-
-const inputFile = path.resolve(process.cwd(), inputArg);
-const outputDirectory = path.resolve(process.cwd(), outputArg);
-
-writeClient(inputFile, outputDirectory).catch(error => {
-  console.error(error);
-  process.exit(1);
-});
+    writeClient(inputFile, outputDirectory).catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+  })
+  .parse(process.argv);
