fc255267e49e909759958da05bd7f5f208af2b39
Benny Neugebauer <bn@bennyn.de>
2019-05-26 21:17:00 +0200

    feat: Support OpenAPI specs from URL

--- src/cli.ts
+++ src/cli.ts
@@ -12,8 +12,8 @@ program
   .name(binName)
   .description(description)
   .version(version, '-v, --version')
-  .option('-i, --input <file>', 'Set the input file (required)')
-  .option('-o, --output <directory>', 'Set the output directory (required)')
+  .option('-i, --input <file>', 'File path (or URL) to OpenAPI Specification, i.e. swagger.json (required)')
+  .option('-o, --output <directory>', 'Path to output directory for generated TypeScript code (required)')
   .parse(process.argv);
 
 if (!program.input || !program.output) {
@@ -21,7 +21,7 @@ if (!program.input || !program.output) {
   process.exit(1);
 }
 
-const inputFile = path.resolve(process.cwd(), program.input);
+const inputFile = program.input.startsWith('http:') ? program.input : path.resolve(process.cwd(), program.input);
 const outputDirectory = path.resolve(process.cwd(), program.output);
 
 writeClient(inputFile, outputDirectory)
