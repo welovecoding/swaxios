d6e8675b4a1baa1a014b550842f38a8a24dc3957
Florian Keller <florian.keller@wire.com>
2019-05-27 14:04:55 +0200

    fix test

--- src/util/FileUtil.test.ts
+++ src/util/FileUtil.test.ts
@@ -3,8 +3,8 @@ import * as FileUtil from './FileUtil';
 describe('getUniqueFileName', () => {
   it('creates unique file names', () => {
     const fileName = FileUtil.getUniqueFileName('MyFile');
-    const fileName1 = FileUtil.getUniqueFileName('MyFile1');
-    const fileName2 = FileUtil.getUniqueFileName('MyFile2');
+    const fileName1 = FileUtil.getUniqueFileName('MyFile');
+    const fileName2 = FileUtil.getUniqueFileName('MyFile');
     expect(fileName).toBe(null);
     expect(fileName1).toBe('MyFile1');
     expect(fileName2).toBe('MyFile2');
