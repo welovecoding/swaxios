b152ceb66e4f2cdc5f5a747937176a0bae9eca3f
Benny Neugebauer <bn@bennyn.de>
2019-05-29 17:51:05 +0200

    Add E2E test case

--- src/Swaxios.test.ts
+++ src/Swaxios.test.ts
@@ -1,5 +1,33 @@
+import fs from 'fs-extra';
+import os from 'os';
+import path from 'path';
 import {Spec} from 'swagger-schema-official';
-import {exportServices} from './Swaxios';
+import {exportServices, writeClient} from './Swaxios';
+
+let tempDir: string;
+
+function createTempDir(prefix: string = 'Swaxios'): Promise<string> {
+  const osTmpDir = os.tmpdir();
+  const fullPath = path.join(osTmpDir, prefix);
+  return fs.mkdtemp(fullPath);
+}
+
+beforeEach(async () => (tempDir = await createTempDir()));
+
+afterEach(() => fs.remove(tempDir));
+
+describe('writeClient', () => {
+  it('generates descriptions', async () => {
+    const inputFile = path.resolve(__dirname, './test/snapshots/1-query-param-description.json');
+    await writeClient(inputFile, tempDir, true);
+    const actual = await fs.readFile(path.join(tempDir, 'rest/instance/ArchiveService.ts'), 'utf-8');
+    const expected = await fs.readFile(
+      path.resolve(__dirname, './test/snapshots/1-query-param-description.ts'),
+      'utf-8'
+    );
+    expect(actual).toBe(expected);
+  });
+});
 
 describe('exportServices', () => {
   it('merges resources if they belong to the same service', async () => {
