68675443a724bfae842b8fdce9d3082bfe2912ff
Florian Keller <ffflorian@users.noreply.github.com>
2019-08-08 23:53:41 +0200

    fix: Use original Swagger object and fix references (#70)

--- src/validator/SwaggerValidator.ts
+++ src/validator/SwaggerValidator.ts
@@ -2,5 +2,8 @@ import {OpenAPIV2} from 'openapi-types';
 import SwaggerParser from 'swagger-parser';
 
 export async function validateConfig(swaggerJson: OpenAPIV2.Document): Promise<void> {
-  await SwaggerParser.validate(swaggerJson);
+  // let's create a copy because swagger-parser modifies the object,
+  // see https://github.com/APIDevTools/swagger-parser/issues/77
+  const swaggerJsonCopy = JSON.parse(JSON.stringify(swaggerJson));
+  await SwaggerParser.validate(swaggerJsonCopy);
 }
